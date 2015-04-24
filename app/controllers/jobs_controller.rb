d659d5cbe0aab70fb139d77c31472f49701f5b5c
Amy <amyrhunter@gmail.com>
2015-04-24 15:42:15 -0700

    Adds update to jobs controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -16,6 +16,11 @@ class JobsController < ApplicationController
 		@job = Job.find(params[:id])
 	end
 
+	def update
+  		@job = Job.find(params[:id])
+  		@job.update_attributes(job_params)
+  		redirect_to jobs_path
+	end
 	private
 
 	def job_params
